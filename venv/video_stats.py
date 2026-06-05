9b174e2907103a9bdaf625676920d68364978d64
v-git379 <vijayasree.3779@gmail.com>
2026-06-04 22:08:11 -0700

    project

--- venv/video_stats.py
+++ venv/video_stats.py
@@ -8,6 +8,7 @@ load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
 
 
 def get_playlist_id():
@@ -34,5 +35,47 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
     
+def get_video_ids(playlist_id):
+
+    video_ids = []
+
+    pageToken = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlist_id}&key={API_KEY}"
+
+    try:
+
+
+            while True:
+                url = base_url
+                if pageToken:
+                    url += f"&pageToken={pageToken}"
+    
+                response = requests.get(url)
+    
+                response.raise_for_status()
+    
+                data = response.json()
+    
+                for item in data.get('items', []):
+                    video_id = item['contentDetails']['videoId']
+                    video_ids.append(video_id)
+    
+                pageToken = data.get('nextPageToken')
+    
+                if not pageToken:
+                    break
+    
+            #print(video_ids)
+    
+            return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+
+
+
 if __name__ == "__main__":
-   get_playlist_id()
+   playlist_id = get_playlist_id()
+   get_video_ids(playlist_id)
