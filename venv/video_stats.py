cbe9e9c9590bc0cfa214aa9d6af4f7e02a1b0d14
v-git379 <vijayasree.3779@gmail.com>
2026-06-04 23:23:40 -0700

    project

--- venv/video_stats.py
+++ venv/video_stats.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+from datetime import date
 import os
 from dotenv import load_dotenv
 
@@ -73,9 +73,58 @@ def get_video_ids(playlist_id):
     except requests.exceptions.RequestException as e:
         raise e
     
+def batch_list(video_id_lst, batch_size):
+    for video_id in range(0, len(video_id_lst), batch_size):
+        yield video_id_lst[video_id:video_id + batch_size]
 
+'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id=0e3GPea1Tyg&key=[YOUR_API_KEY]'
 
+def extarct_video_data(video_ids):
+    extracted_data = []
+
+    try: 
+        for batch in batch_list(video_ids, maxResults):
+            video_ids_str = ",".join(batch)
+
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}" 
+            
+            response = requests.get(url)
+    
+            response.raise_for_status()
+    
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+            
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet['title'],
+                    "publishedAt": snippet['publishedAt'],
+                    "duration": contentDetails['duration'],
+                    "viewCount": statistics.get('viewCount', None),
+                    "likeCount": statistics.get('likeCount', None),
+                    "commentCount": statistics.get('commentCount', None)
+                }
+            
+                extracted_data.append(video_data)
+
+        return extracted_data
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+def save_to_json(extracted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
 
 if __name__ == "__main__":
    playlist_id = get_playlist_id()
-   get_video_ids(playlist_id)
+   video_ids = get_video_ids(playlist_id)
+   video_data = extarct_video_data(video_ids)
+   save_to_json(video_data)
+   
